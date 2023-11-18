1f34952ee7036a80ea15be7d4d07aa3ce90ab338
yushanlu <yushanlu1031@gmail.com>
2023-11-18 09:18:17 +0000

    90/100; comments added; TODO: error handling

--- nb/server.py
+++ nb/server.py
@@ -6,20 +6,25 @@ import station_pb2
 import grpc
 
 """
-Server for gRPC. Why use cassandra for the table?
+Implements the interface from station_pb2_grpc.StationServicer
+RecordTemps will insert new temperature highs/lows to weather.stations
+StationMax will return the maximum tmax ever seen for the given station
+
+Why use cassandra for the table?
 weather.stations table looks like this:
-    id         date         station_record
+    id         date      station_record {tmin, tmax}
 ACW00011604   20200201       {0, 100}
               20200202       {-1, 99} 
               20200203       {-3, 101}
 
-PRIMARY KEY ((id), date): each id (parition key) and date(cluster key) will correspond to a unique row.
-Each row is a partition. Use cassandra to ensure each partition is stored in one machine.
-The table created in note book does not have date and name.
-Use RecordTemps (given id, date, station_record) to fill in the tables.
+PRIMARY KEY ((id), date): each id (parition key) and date(cluster key) will correspond to a unique row
+Each row is a partition. Use cassandra to ensure each partition is stored in one machine
+The table created in notebook does not have date and station_record
+Use RecordTemps (given id, date, station_record) to fill in the tables
 Use StationMax to iterate/sort the station_record to find the largest tmax ever for this station
 
 """
+
 cluster = Cluster(['p6-db-1', 'p6-db-2', 'p6-db-3'])
 cass = cluster.connect()
 cass.execute("USE weather")
@@ -42,7 +47,8 @@ class ModelServer(station_pb2_grpc.StationServicer):
             INSERT INTO stations (id, date, record)
             VALUES(?, ?, ?) 
             """)
-            # ?, ?, ? will correspond to (request.station, request.date, Record(request.tmin, request.tmax)) at line 40
+            # ?, ?, ? will correspond to (request.station, request.date, Record(request.tmin, request.tmax)) at line 72
+            # Note that W = 1 (ConsistencyLevel.ONE) because we prioritize high write availability
             self.insert_statement.consistency_level = ConsistencyLevel.ONE
 
             self.max_statement = cass.prepare("""
@@ -50,7 +56,7 @@ class ModelServer(station_pb2_grpc.StationServicer):
             FROM stations
             WHERE id = ?
             """)
-            self.max_statement.consistency_level = ConsistencyLevel.THREE
+            self.max_statement.consistency_level = ConsistencyLevel.THREE # Choose R so that R + W > RF
 
     def RecordTemps(self, request, context):
         """
@@ -76,12 +82,14 @@ class ModelServer(station_pb2_grpc.StationServicer):
         Given the request (only has id), return the largest tmax ever seen
         """
         try:
-            rows = cass.execute(self.max_statement, [request.station]) # turning the df in to a list; the argument for ? must be a list, even if there's only one
+            rows = cass.execute(self.max_statement, [request.station]) # turning the df into a list; the argument for ? must be a list, even if there's only one
             sorted_rows = sorted(rows, key=lambda x: x.record.tmax, reverse=True) # sort the list based on tmax
-            return station_pb2.StationMaxReply(tmax=sorted_rows[0].record.tmax, error="") # sorted_rows[0] will be the first row of the station id
+            return station_pb2.StationMaxReply(tmax=sorted_rows[0].record.tmax, error="") # sorted_rows[0] will be the first row of the given station id
 
         except Exception as e:
-            ##TODO: modfiy the error when there's not enough node
+            #TODO: modfiy the error when there's not enough node??
+            #TODO: We want to avoid a situation where a StationMax returns a smaller temperature than one previously added with RecordTemps
+            # it would be better to return an error message if necessary
             return station_pb2.StationMaxReply(error=str(e))
 
 if __name__ == "__main__":
