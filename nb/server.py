df1a3d210589966513b00f83c961dcd264ab0a8d
yushanlu <yushanlu1031@gmail.com>
2023-11-18 09:10:07 +0000

    minor fixes; 90/100; TODO: error handling

--- nb/server.py
+++ nb/server.py
@@ -5,13 +5,30 @@ import station_pb2_grpc
 import station_pb2
 import grpc
 
+"""
+Server for gRPC. Why use cassandra for the table?
+weather.stations table looks like this:
+    id         date         station_record
+ACW00011604   20200201       {0, 100}
+              20200202       {-1, 99} 
+              20200203       {-3, 101}
 
+PRIMARY KEY ((id), date): each id (parition key) and date(cluster key) will correspond to a unique row.
+Each row is a partition. Use cassandra to ensure each partition is stored in one machine.
+The table created in note book does not have date and name.
+Use RecordTemps (given id, date, station_record) to fill in the tables.
+Use StationMax to iterate/sort the station_record to find the largest tmax ever for this station
+
+"""
 cluster = Cluster(['p6-db-1', 'p6-db-2', 'p6-db-3'])
 cass = cluster.connect()
 cass.execute("USE weather")
 
 class Record(object):
-
+    """
+    This class helps inserting station_type, which is a user defined typed in notebook:
+    cass.execute("CREATE TYPE station_record (tmin int, tmax int)")
+    """
     def __init__(self, tmin, tmax):
         self.tmin = tmin
         self.tmax = tmax
@@ -20,10 +37,12 @@ cluster.register_user_type('weather', 'station_record', Record)
 
 class ModelServer(station_pb2_grpc.StationServicer):
     def __init__(self):
+            # initilize (prepare) here so the statement can be stored in cached; better for reuse
             self.insert_statement = cass.prepare("""
             INSERT INTO stations (id, date, record)
-            VALUES(?, ?, ?)
+            VALUES(?, ?, ?) 
             """)
+            # ?, ?, ? will correspond to (request.station, request.date, Record(request.tmin, request.tmax)) at line 40
             self.insert_statement.consistency_level = ConsistencyLevel.ONE
 
             self.max_statement = cass.prepare("""
@@ -34,20 +53,35 @@ class ModelServer(station_pb2_grpc.StationServicer):
             self.max_statement.consistency_level = ConsistencyLevel.THREE
 
     def RecordTemps(self, request, context):
+        """
+        Given the request, fill in the weather.stations table
+        gRPC fucntion:
+        request.
+            station: str
+            date: str
+            tmin: int
+            tmax: int
+        """
         try:
+            # execute the prepare statement
             cass.execute(self.insert_statement, (request.station, request.date, Record(request.tmin, request.tmax)))
             return station_pb2.RecordTempsReply(error="") 
 
         except Exception as e:
+            #TODO: modfiy the error when there's not enough node
             return station_pb2.RecordTempsReply(error=str(e))
 
     def StationMax(self, request, context):
+        """
+        Given the request (only has id), return the largest tmax ever seen
+        """
         try:
-            rows = cass.execute(self.max_statement, [request.station])
-            sorted_rows = sorted(rows, key=lambda x: x.record.tmax, reverse=True)
-            return station_pb2.StationMaxReply(tmax=sorted_rows[0].record.tmax, error="")
+            rows = cass.execute(self.max_statement, [request.station]) # turning the df in to a list; the argument for ? must be a list, even if there's only one
+            sorted_rows = sorted(rows, key=lambda x: x.record.tmax, reverse=True) # sort the list based on tmax
+            return station_pb2.StationMaxReply(tmax=sorted_rows[0].record.tmax, error="") # sorted_rows[0] will be the first row of the station id
 
         except Exception as e:
+            ##TODO: modfiy the error when there's not enough node
             return station_pb2.StationMaxReply(error=str(e))
 
 if __name__ == "__main__":
